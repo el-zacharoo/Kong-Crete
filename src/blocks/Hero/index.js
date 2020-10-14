5f161ffa0c5261b9397159b4909e60ed5971b258
Zachary Weston <zachary.weston@propellerhead.co.nz>
2020-10-14 13:25:40 +1300

    removed unused component

--- src/blocks/Hero/index.js
+++ src/blocks/Hero/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
